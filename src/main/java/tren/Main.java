a1d62dda024787f6cd666da34fb1dd6a8d0411f4
Antonio Borrego <antonioborreg@gmail.com>
2025-10-20 19:10:15 +0200

    añadir dependencias

--- src/main/java/tren/Main.java
+++ src/main/java/tren/Main.java
@@ -2,4 +2,8 @@ package tren;
 
 public class Main
 {
+    public static void main(String[] args)
+    {
+        Vagon vagon = new Vagon(100);
+    }
 }
--- src/main/java/tren/Vagon.java
+++ src/main/java/tren/Vagon.java
@@ -2,5 +2,29 @@ package tren;
 
 public class Vagon
 {
+    private int capacidad;
+    private int carga;
 
+    public Vagon(int capacidad)
+    {
+        this.capacidad = capacidad;
+        this.carga = 0;
+    }
+
+    public int carga(int ton)
+    {
+        int cabe = capacidad-carga;
+        int sobra = 0;
+
+        if (ton >= cabe)
+        {
+            carga = capacidad;
+            sobra = ton-cabe;
+        }
+        else
+        {
+            carga += ton;
+        }
+    return sobra;
+    }
 }
